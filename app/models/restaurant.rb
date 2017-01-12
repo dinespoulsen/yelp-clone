2290f295b579109d8c9cdc17c2331a9207816b07
George Eaton <george_eaton@hotmail.co.uk>
2017-01-12 11:20:38 +0000

    adds WithUserExtension Module

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,19 +1,10 @@
 class Restaurant < ApplicationRecord
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  has_many :reviews do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||= user
-      build(attributes)
-    end
-  end
-
+  has_many :reviews,
+    -> { extending WithUserAssociationExtension },
+    dependent: :destroy
 
   belongs_to :user
 
-  def build_review(attributes = {}, user)
-    review = reviews.build(attributes)
-    review.user = user
-    review
-  end
 end
