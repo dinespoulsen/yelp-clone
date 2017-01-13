5bff018e32a9fb403b966ed52acb0ed67949017d
Dines Poulsen <dinespoulsen84@gmail.com>
2017-01-13 11:54:49 +0000

    Added aws region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,9 @@ Rails.application.configure do
     :storage => :s3,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_Id'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :s3_region => ENV['AWS_REGION']
     }
   }
   # Code is not reloaded between requests.
